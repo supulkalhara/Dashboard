ae98f07a18896b7cbce273af177ee176d8c50446
Supul Kalhara <supul9229kalhara@gmail.com>
2022-06-05 17:55:39 +0530

    beautified

--- Views/about.py
+++ Views/about.py
@@ -3,25 +3,23 @@ from churn import *
 import matplotlib.pyplot as plt
 import plotly.figure_factory as ff
 import plotly.graph_objects as go
+import plotly.express as px
 import seaborn as sns
 
 def show_correlations(dataframe, show_chart = True):
     fig = plt.figure()
     corr = dataframe.corr()
     if show_chart == True:
-        sns.heatmap(corr, 
-                    xticklabels=corr.columns.values,
-                    yticklabels=corr.columns.values,
-                    annot=False)
+        fig = px.imshow(corr,
+                        text_auto=True,
+                        width=600,
+                        height=600)
     return fig
 
 def feature_importance(X, rf_model):
     importances = rf_model.feature_importances_
-    std = np.std([tree.feature_importances_ for tree in rf_model.estimators_], axis=0)
     forest_importances = pd.Series(importances, index=X.columns)
-    fig, ax = plt.subplots()
-    forest_importances.plot.bar(yerr=std, ax=ax)
-    ax.set_ylabel("Mean decrease in impurity")
+    fig = st.bar_chart(forest_importances)
     return fig
 
 def aboutView():    
@@ -35,7 +33,7 @@ def aboutView():
             
     st.subheader("Correlations")
     st.write("Select the features that you need to see correlation")
-    col1, col2, col3, col4 = st.columns((1, 1, 1, .5))
+    col1, col2, plots , col5 = st.columns((1, 0.5, 4, 3))
     
     col = list(pre_processed_dataset_train.columns)
     correlation_df = show_correlations(pre_processed_dataset_train[col],show_chart=True)
@@ -45,26 +43,26 @@ def aboutView():
         'Feature 1',
         (pre_processed_dataset_train.columns))   
 
-    option2 = col2.selectbox(
+    option2 = col1.selectbox(
         'Feature 2',
         (pre_processed_dataset_train.columns))
     
     result = pre_processed_dataset_train[option].corr(pre_processed_dataset_train[option2])
     
     if (result < 0.5):
-        col3.metric("Correlation", "{:.2f}".format(result), "- low")
+        col2.metric("Correlation", "{:.2f}".format(result), "- low")
     else:
-        col3.metric("Correlation", "{:.2f}".format(result), "+ high")
+        col2.metric("Correlation", "{:.2f}".format(result), "+ high")
         
-    
-    with col4.expander(""):
-        col4.write("See heatmap:")
-        col4.write(correlation_df)
+    col5.plotly_chart(correlation_df, use_container_width=True)
         
 #--------------------------------------
-    st.subheader("Feature Analysis")
-    st.write("Select the features that you need to see relation")
-    col1, col2, col3, col4 = st.columns(4)
+    col1.write("")
+    col1.write("")
+    col1.write("")
+    col1.subheader("Feature Analysis")
+    col1.write("Select the features that you need to see relation")
+    # col1, col2, col3, col4 = st.columns(4)
     
     option3 = col1.selectbox(
         'Graph type',
@@ -72,35 +70,36 @@ def aboutView():
          'Histogram', 
          'Line Chart', 
          'Area plot', 
-         ) # 'Scatter plot'
+         'Scatter plot'
+         ) # 
     )   
         
     
     if (option3 == 'Bar plot'):
-        option4 = col2.selectbox(
+        option4 = col1.selectbox(
         'Feature 1:',
         (num_features))  
         
-        type = st.radio(
+        type = col1.radio(
             "Do you want to plot 2 fetures?",
             ('No', 'Yes'))
         if type == 'No':
-            st.bar_chart(pre_processed_dataset_train[option4])
+            plots.bar_chart(pre_processed_dataset_train[option4])
         elif type == 'Yes':
-            option5 = col3.selectbox(
+            option5 = col1.selectbox(
             'Feature 2:',
             (num_features))
             
             if (option4 != option5):
-                # chart_data = pd.DataFrame(columns=[option4, option5])
+    
                 chart_data = [pre_processed_dataset_train[option4], pre_processed_dataset_train[option5]]
-                st.bar_chart(chart_data)
+                plots.bar_chart(chart_data)
             else:
                 original_title = '<p style="font-family:Courier; color:red; font-size: 15px;">You cannot select the same feature</p>'
-                st.markdown(original_title, unsafe_allow_html=True)
+                plots.markdown(original_title, unsafe_allow_html=True)
 
     elif (option3 == 'Histogram'):
-        option4 = col2.selectbox(
+        option4 = col1.selectbox(
         'Feature 1:',
         (num_features))  
         
@@ -109,10 +108,10 @@ def aboutView():
             ('No', 'Yes'))
         if type == 'No':
             fig = ff.create_distplot([pre_processed_dataset_train[option4]], [option4])
-            st.plotly_chart(fig, use_container_width=True)
+            plots.plotly_chart(fig, use_container_width=True)
             
         elif type == 'Yes':
-            option5 = col3.selectbox(
+            option5 = col1.selectbox(
             'Feature 2:',
             (num_features))
             
@@ -124,17 +123,17 @@ def aboutView():
                 
                 hist_data = [x1, x2]
                 fig = ff.create_distplot(hist_data, group_labels, bin_size=[.1, .25, .5])
-                st.plotly_chart(fig, use_container_width=True)
+                plots.plotly_chart(fig, use_container_width=True)
             else:
                 original_title = '<p style="font-family:Courier; color:red; font-size: 15px;">You cannot select the same feature</p>'
                 st.markdown(original_title, unsafe_allow_html=True)
         
     elif (option3 == 'Line Chart') :
-        option4 = col2.selectbox(
+        option4 = col1.selectbox(
         'Feature 1:',
         (num_features))  
 
-        option5 = col3.selectbox(
+        option5 = col1.selectbox(
         'Feature 2:',
         (num_features))
         
@@ -145,45 +144,36 @@ def aboutView():
             
             chart_data = [x1, x2]
             fig = ff.create_distplot(chart_data, group_labels)
-            st.plotly_chart(fig)
+            plots.plotly_chart(fig)
         else:
             original_title = '<p style="font-family:Courier; color:red; font-size: 15px;">You cannot select the same feature</p>'
             st.markdown(original_title, unsafe_allow_html=True)
         
     elif (option3 == 'Area plot'):
-        st.area_chart(pre_processed_dataset_train)
-        
+        plots.area_chart(pre_processed_dataset_train)
         
     elif (option3 == 'Scatter plot'):
-        option4 = col2.selectbox(
+        option4 = col1.selectbox(
         'Feature 1:',
         (num_features))  
         
-        type = st.radio(
-            "Do you want to plot 2 fetures?",
-            ('No', 'Yes'))
-        if type == 'No':
-            fig = ff.create_distplot([pre_processed_dataset_train[option4]], [option4])
-            st.map(fig)
-            
-        elif type == 'Yes':
-            option5 = col3.selectbox(
-            'Feature 2:',
-            (num_features))
-            
-            if (option4 != option5):
-                # x1 = pre_processed_dataset_train[option4]
-                # x2 = pre_processed_dataset_train[option5]
-                group_labels = [option4, option5]
-                chart_data = [pre_processed_dataset_train[option4], pre_processed_dataset_train[option5]]
-                fig = ff.create_distplot(chart_data, group_labels)
-                st.map(fig)
-            else:
-                original_title = '<p style="font-family:Courier; color:red; font-size: 15px;">You cannot select the same feature</p>'
-                st.markdown(original_title, unsafe_allow_html=True)
+        option5 = col1.selectbox(
+        'Feature 2:',
+        (num_features))
+        
+        if (option4 != option5):
+            x1 = pre_processed_dataset_train[option4]
+            x2 = pre_processed_dataset_train[option5]
+            group_labels = [option4, option5]
+            fig = px.scatter(pre_processed_dataset_train,
+                             x=option4,
+                             y=option5,
+            )
+            plots.plotly_chart(fig)
+        else:
+            original_title = '<p style="font-family:Courier; color:red; font-size: 15px;">You cannot select the same feature</p>'
+            plots.markdown(original_title, unsafe_allow_html=True)
                 
-
-
     st.subheader("Feature Importance")   
     st.write("See feature importance using random forest:")
     st.write(feature_importance(X, rf_model))
